05f539eddacbd4233a25e182a9594abb61f76e89
fotocopia <catalina.pimentel@uniacc.edu>
2019-09-25 15:24:47 -0300

    first commit OPERACIONES

---
+++ JavaOperationsGIT/src/javaoperationsgit/OperacionesBasicas.java
@@ -0,0 +1,17 @@
+package javaoperationsgit;
+
+public class OperacionesBasicas {
+
+    public void sumar(int a, int b) {
+    }
+
+    public void restar(int a, int b) {
+    }
+
+    public void multiplicar(int a, int b) {
+    }
+
+    public void dividir(int a, int b) {
+    }
+
+}
